d85a5d9ad8aad005c84a44a113171c8288028b28
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-06-16 23:15:45 +0100

    add filter block

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -3,4 +3,10 @@ module MyEnumerable
     each { |item| return false unless block.call(item) }
     true
   end
+
+  def filter(&block)
+    arr = []
+    each { |item| arr << item if block.call(item) }
+    arr
+  end
 end
