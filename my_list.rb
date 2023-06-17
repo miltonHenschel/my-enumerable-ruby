ee7f64e8b068992deefad3af524e31ef8b509ea1
TUMBU BARTHOLOMEW ELAD <tumbu.elad@gmail.com>
2023-06-17 13:52:28 -0700

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -2,10 +2,11 @@ require_relative 'my_enumerable'
 
 class MyList
   include MyEnumerable
-
+  
   def initialize(*list)
     @list = list
   end
+  
   def each(&block)
     @list.each(&block)
   end
